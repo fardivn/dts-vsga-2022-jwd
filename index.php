a765102b24962a29b2734a93c13fb722915334fa
fardivn <farahdivanabila5@gmail.com>
2022-07-22 23:19:39 +0700

    adding a php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tugas Git dan Github</title>
+</head>
+<body>
+	<h1>Tugas Git dan Github</h1>
+	<p>Nama: Farah Diva Nabila</p>
+</body>
+</html>
